a9bd9e6109e3f403c5872036a862b68a0b4395b0
SalZeus <54487998+SalZeus@users.noreply.github.com>
2023-04-27 01:55:57 +0000

    adding planet and character PlanetCard

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -4,21 +4,20 @@ import { Link } from "react-router-dom";
 export const Navbar = () => {
 	return (
 		<nav className="navbar navbar-light bg-light mb-3">
-			<div class="logo">
-				<div class="title tc-yellow-400">star</div>
-				<div class="subtitle">A VISUAL GUIDE</div>
-				<div class="title tc-yellow-400">wars</div>
+			<div className="logo">
+				<div className="title tc-yellow-400">Star Wars!</div>
+				<div className="subtitle">A fancy Guide x.x</div>
 			</div>
-			<div class="dropstart btn-group ml-auto pe-5">
-				<button type="button" class="btn btn-info dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
-					Action
+			<div className="dropstart btn-group ml-auto pe-5">
+				<button type="button" className="btn btn-info dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+					<strong>Favorites UwU</strong>
 				</button>
-				<ul class="dropdown-menu">
-					<li><a class="dropdown-item" href="#">Action</a></li>
-					<li><a class="dropdown-item" href="#">Another action</a></li>
-					<li><a class="dropdown-item" href="#">Something else here</a></li>
-					<li><hr class="dropdown-divider"/></li>
-					<li><a class="dropdown-item" href="#">Separated link</a></li>
+				<ul className="dropdown-menu">
+					<li><a className="dropdown-item" href="#">Fav1</a></li>
+					<li><a className="dropdown-item" href="#">Fav 2</a></li>
+					<li><a className="dropdown-item" href="#">Fav 3</a></li>
+					<li><hr className="dropdown-divider"/></li>
+					<li><a className="dropdown-item" href="#">Clear All</a></li>
 				</ul>
 				</div>
 		</nav>
