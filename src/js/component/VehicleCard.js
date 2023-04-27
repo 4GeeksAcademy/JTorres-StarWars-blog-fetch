014d223823e1e836da16da88f02646962e5747ee
SalZeus <54487998+SalZeus@users.noreply.github.com>
2023-04-27 02:53:45 +0000

    adding vehicles and style changes

---
+++ src/js/component/VehicleCard.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+
+export const VehicleCard = () =>(
+    <div className="wholecard card w-50 h-25">
+        <img src="https://fastly.picsum.photos/id/965/400/200.jpg?hmac=pUxpiyGtnQOmA8jpiS9pzxoiFMbLfAX7d590H8q-OOI" className="card-img-top" alt="CharacterImg"></img>
+        <div className="card-body">
+            <h5 className="card-title">Vehicle Name</h5>
+            <p className="card-text">Nerdy stuffs</p>
+        </div>
+        <ul className="list-group list-group-flush">
+            <li className="list-group-item">Trait 1</li>
+            <li className="list-group-item">Trait 2</li>
+            <li className="list-group-item">Trait 3</li>
+        </ul>
+        <div className="card-body ms-auto px-auto">
+            <a href="#" className="btn btn-outline-info mx-4">Nerd mode Here</a>
+            <a href="#" className="btn btn-outline-warning">♡</a>
+        </div>
+    </div>
+)
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -2,6 +2,7 @@ import React from "react";
 import "../../styles/home.css";
 import { CharacterCard } from "../component/CharacterCard";
 import { PlanetCard } from "../component/PlanetCard";
+import { VehicleCard } from "../component/VehicleCard";
 
 export const Home = () => (
 	<>
@@ -12,6 +13,10 @@ export const Home = () => (
 		<h1>Planets</h1>
 		<div className="container overflow-scroll my-3">
 			<PlanetCard/>
+		</div>
+		<h1>Vehicles</h1>
+		<div className="container overflow-scroll my-3">
+			<VehicleCard/>
 		</div>
 	</>
 );
